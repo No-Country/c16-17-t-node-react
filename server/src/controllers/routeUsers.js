1ebc0010b735f0130d533b4d14cd399b444374b6
Maximiliano Van Megroot <maximilianovanmegroot@gmail.com>
2024-02-23 12:01:01 -0300

    Back (#10)
    * A101 - feat: post-users
    * A201 - feat: post-users/login
    * A201 - feat: get-users/:id
    * B204 - feat: post-pets
    * B201 - feat: get-pets/:id 
    * A301 - feat: delete-pets/:id
    * A601 - feat: delete-users/:id

--- server/src/controllers/routeUsers.js
+++ server/src/controllers/routeUsers.js
@@ -9,10 +9,12 @@
 const { Router } = require('express');
 const {
 	createUser,
+	loginUser,
 	deleteUser,
 	searchUser,
 	updateUser,
 } = require('../services/userService');
+const userExtractor = require('../middleware/userExtractor');
 const route = Router();
 
 // Ruta para crear un nuevo usuario
@@ -22,6 +24,8 @@ route.post('/', async (req, res, next) => {
 		const newData = {
 			email: body.email,
 			password: body.password,
+			name: body.name,
+			lastName: body.lastName,
 		};
 		const newUser = await createUser(newData);
 		res.status(201).json(newUser).end();
@@ -34,12 +38,12 @@ route.post('/', async (req, res, next) => {
 route.post('/login', async (req, res, next) => {
 	try {
 		const { body } = req;
-		const newData = {
+		const data = {
 			email: body.email,
 			password: body.password,
 		};
-		const userJWT = { message: 'El JWT' };
-		res.status(201).json(userJWT).end();
+		const user = await loginUser(data);
+		res.status(201).json(user).end();
 	} catch (err) {
 		next(err);
 	}
@@ -62,10 +66,10 @@ route.post('/password', async (req, res, next) => {
 });
 
 // Ruta para obtener un usuario por ID
-route.get('/:id', async (req, res, next) => {
+route.get('/:id', userExtractor, async (req, res, next) => {
 	try {
-		const { id } = req.params;
-		const user = await searchUser(id);
+		const { userId } = req.user;
+		const user = await searchUser(userId);
 		res.status(200).json(user).end();
 	} catch (err) {
 		next(err);
@@ -90,10 +94,11 @@ route.put('/:id', async (req, res, next) => {
 });
 
 // Ruta para eliminar un usuario por ID
-route.delete('/:id', async (req, res, next) => {
+route.delete('/:id', userExtractor, async (req, res, next) => {
 	try {
+		const { userId } = req.user;
 		const { id } = req.params;
-		const data = await deleteUser(id);
+		const data = await deleteUser({ id, userId });
 		res.status(200).json(data).end();
 	} catch (err) {
 		next(err);
