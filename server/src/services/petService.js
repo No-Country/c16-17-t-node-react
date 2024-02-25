7fe771c000cae25d659d6032fcdb6f505347a907
MaxiV95 <maximilianovanmegroot@gmail.com>
2024-02-24 21:11:39 -0300

    fix: update-pets

--- server/src/services/petService.js
+++ server/src/services/petService.js
@@ -5,29 +5,46 @@
  * y la conexión con la base de datos.
  */
 
-const petSchema = require('../models/petSchema');
+const petModel = require('../models/petModel');
+const userModel = require('../models/userModel');
+const {
+	IncorrectData,
+	ValidationError,
+} = require('../middleware/customErrors');
 
-const createPet = async (newData) => {
-	const newPet = await petSchema.createPet(newData);
+const createPet = async ({ owner, ...newData }) => {
+	const user = await userModel.findById(owner);
+	if (!user) throw new IncorrectData('The "User" field must reference a valid UserID.');
+	const newPet = await this.model('Pet').create({ owner, ...newData });
+	await userModel.findByIdAndUpdate(user._id, { $push: { pets: newPet._id } });
 	return newPet;
 };
 
 const searchPet = async (id) => {
-	const pet = await petSchema.findById(id).populate('owner', 'name telephone');
+	const pet = await petModel.findById(id).populate('owner', 'name telephone');
 	return pet;
 };
 
-const updatePet = async (id, updateData) => {
-	const updatedPet = await petSchema.findByIdAndUpdate(id, updateData, {
+const updatePet = async ({ owner, id, ...updateData }) => {
+	const pet = await petModel.findById(id);
+	if (!pet) throw new IncorrectData(`The pet with id ${id} was not found.`);
+	if (pet.owner != owner)	throw new ValidationError('Insufficient permissions.');
+	const updatedPet = await petModel.findByIdAndUpdate(id, updateData, {
 		new: true,
 		runValidators: true,
 	});
 	return updatedPet;
 };
 
-const deletePet = async (petData) => {
-	const data = await petSchema.deletePet(petData);
-	return data;
+const deletePet = async ({ owner, id }) => {
+	const pet = await petModel.findById(id);
+	if (!pet) throw new IncorrectData(`The pet with id ${id} was not found.`);
+	if (pet.owner != owner)
+		throw new ValidationError('Insufficient permissions.');
+	const User = require('./userSchema');
+	await User.findOneAndUpdate({ _id: pet.owner }, { $pull: { pets: id } });
+	const isDelete = await petModel.deleteOne({ _id: id });
+	return isDelete;
 };
 
 module.exports = { createPet, searchPet, updatePet, deletePet };
