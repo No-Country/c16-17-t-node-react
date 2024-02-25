160ee0f62757dea141724a1e948073fd4c4b5d92
rolando22 <rolando2207@gmail.com>
2024-02-24 21:06:18 -0300

    feat: button component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { AppRoutes } from './routes/AppRoutes';
 import PrivateRoute from './routes/PrivateRoutes';
 import { FormLogin } from './components';
 import { ErrorPage, Home } from './pages';
+import { Button } from './components/Button/Button';
 
 
 
 export function App() {
-	
+
 	return (
-		<BrowserRouter>
+    <BrowserRouter>
 			<AppRoutes/>
+      <Button color={'success'}>Ingresar</Button>
 		</BrowserRouter>
 	);
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { PetForm } from './PetProfile/PetForm'
 export { FormLogin } from './formLogin/FormLogin'
+export { Button } from './Button/Button';
