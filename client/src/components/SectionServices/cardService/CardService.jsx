ccb629b82ceef92d1f38b9912219a487cec6f3c9
Matias Saade <matiassaade@gmail.com>
2024-03-07 18:10:05 -0300

    fix: fixed buttons links, removed wrap from services card on home

--- client/src/components/SectionServices/cardService/CardService.jsx
+++ client/src/components/SectionServices/cardService/CardService.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const CardService = ({img, title, text}) => {
   return (
-    <article className="w-72 h-72 md:h-96 border rounded-xl bg-bgGray flex flex-col items-center justify-start md:gap-7 gap-5 p-5">
-        <figure className="w-full h-1/3 rounded-md p-3 flex justify-center items-center bg-bgBtn">
+    <article className="w-72 lg:max-w-72 md:w-1/3 h-72 md:h-96 rounded-xl bg-bgGray flex flex-col items-center justify-start md:gap-7 gap-5 p-5">
+        <figure className="w-full h-1/3 sm:max-h-24 rounded-md p-3 flex justify-center items-center bg-bgBtn">
             <img
                 className='w-[70px] h-fit'
                 src={img} alt={title}/>
